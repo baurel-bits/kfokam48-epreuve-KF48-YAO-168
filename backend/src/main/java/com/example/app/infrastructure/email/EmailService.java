5bc38fdf061d5016dc3fcae0c5af0fcf9b005db0
Domguia Bemmo Baurel <baurel.bemmo@gmail.com>
2026-09-25 11:20:52 +0100

    feat: fin d'implementation du perimetre v0.1

--- backend/src/main/java/com/example/app/infrastructure/email/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.infrastructure.email;
-
-public interface EmailService {
-
-    void sendSimpleEmail(String to, String subject, String content);
-
-    void sendHtmlEmail(String to, String subject, String htmlContent);
-}
